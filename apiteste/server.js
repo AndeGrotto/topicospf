2a72df6c3fea5d64a977fcf63fbc84762eea5316
Grotto <182994@lci-14-28.UPF>
2022-03-11 21:03:35 -0300

    Commit de 11/03/2022-21h03m35s and 21:03:35,12

--- apiteste/server.js
+++ apiteste/server.js
@@ -10,16 +10,20 @@ const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 const uri = "mongodb://admin:admin@localhost:27018/teste?authSource=teste ";
 MongoClient.connect(uri, (err, client) => {
- if (err)
- return console.log(err);
- db = client.db('teste');
- app.listen(3000, function() { // subir serviço da api na porta 3000
- console.log('API rodando na porta 3000');
- console.log('Testar por http://localhost:3000');
- });
- });
+    if (err)
+        return console.log(err);
+    db = client.db('teste');
+    app.listen(3000, function () { // subir serviço da api na porta 3000
+        console.log('API rodando na porta 3000');
+        console.log('Testar por http://localhost:3000');
+    });
+});
 
- // prerarar para responder ao GET
+// prerarar para responder ao GET
 app.get('/', (req, res) => {
     res.send('Atendida a requisição GET!!');
-   }); 
+});
+
+//rotas para animais
+const animais = require('./animais');
+animais(app);
