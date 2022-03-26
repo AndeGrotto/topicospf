b14742719be58651e922b70302c8267441f4b191
Grotto <182994@lci-14-28.UPF>
2022-03-25 21:58:14 -0300

    Commit de 25/03/2022-21h58m13s and 21:58:13,89

--- apirequisicoes/server/routes/ColaboradorRout.js
+++ apirequisicoes/server/routes/ColaboradorRout.js
@@ -4,5 +4,10 @@ const controle = require('../controller/ColaboradorCont');
 
 // Aqui vão todos os endpoints
 routes.route('/colaboradores').get(controle.listar);
+routes.route('/colaboradores').post(controle.incluir);
+routes.route('/colaboradores').put(controle.alterar);
+routes.route('/colaboradores/:id').delete(controle.excluir);
+routes.route('/colaboradores/:id').get(controle.obterPeloId);
+routes.route('/colaboradores/filtro/:filtro').get(controle.filtrar);
 
 module.exports = routes;
