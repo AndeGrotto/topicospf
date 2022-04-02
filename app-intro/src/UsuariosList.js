2f738206d21d2a17b4bfd979f7d3e959840eeb62
Grotto <182994@lci-14-28.UPF>
2022-04-01 22:08:13 -0300

    Commit de 01/04/2022-22h08m13s and 22:08:13,21

---
+++ app-intro/src/UsuariosList.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function UsuariosList(props) {
+    return (
+        <div className="App de Usuários">
+            <h4>MANUTENÇÃO DE USUÁRIOS</h4>
+
+            <h4>Listagem de usuários</h4>
+            <button onClick={props.onClickAtualizar} type="button"
+                class="btn btn-info btn-sm">Atualizar Lista</button>
+
+            <button onClick={props.onClickInserir} type="button"
+                class="btn btn-primary btn-sm">Inserir</button>
+
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Index</th>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Celular</th>
+                        <th>Operações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.usuarios.length > 0 ? (props.usuarios.map((o, index) => (
+                        <tr key={index}>
+                            <td>{index}</td>
+                            <td>{o.id}</td>
+                            <td>{o.nome}</td>
+                            <td>{o.email}</td>
+                            <td>{o.celular}</td>
+                            <td>
+                                <button type="button"
+                                    class="btn btn-warning btn-sm">Alterar</button>
+                                <button type="button"
+                                    class="btn btn-danger btn-sm">Excluir</button>
+                            </td>
+                        </tr>
+                    ))) : (
+                        <tr>
+                            <td colSpan={5
+                            }>Nenhum usuário.</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+
+
+
+
+
+
+        </div>
+    );
+}
+
+export default UsuariosList;
