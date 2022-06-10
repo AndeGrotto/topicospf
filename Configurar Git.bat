91d135c41557da73652968ed947ce0bcde319ae9
Anderson Grotto <andersongrotto@gmail.com>
2022-06-10 20:34:19 -0300

    Commit de 10/06/2022-20h34m17s and 20:34:17,98

---
+++ Configurar Git.bat
@@ -0,0 +1,6 @@
+git config --global user.name "Anderson Grotto"
+git config --global user.email "andersongrotto@gmail.com"
+git init --initial-branch=main
+git remote add origin https://github.com/AndeGrotto/topicospf.git
+
+timeout 5
